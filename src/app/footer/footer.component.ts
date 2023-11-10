7049702c13211169a6ac0f4aefcebbc81952a7e5
Tuan Nguyen <amtuannguyen@gmail.com>
2023-11-10 07:49:11 -0500

    Add yorkspace theme

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,5 @@
 import { Component, Optional } from '@angular/core';
-import { hasValue } from '../shared/empty.util';
 import { KlaroService } from '../shared/cookies/klaro.service';
-import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
-import { AuthorizationDataService } from '../core/data/feature-authorization/authorization-data.service';
-import { FeatureID } from '../core/data/feature-authorization/feature-id';
 
 @Component({
   selector: 'ds-footer',
@@ -17,22 +12,14 @@ export class FooterComponent {
   /**
    * A boolean representing if to show or not the top footer container
    */
-  showTopFooter = false;
-  showPrivacyPolicy = environment.info.enablePrivacyStatement;
-  showEndUserAgreement = environment.info.enableEndUserAgreement;
-  showSendFeedback$: Observable<boolean>;
+  showTopFooter = true;
+  showPrivacyPolicy = false;
+  showEndUserAgreement = false;
 
-  constructor(
-    @Optional() private cookies: KlaroService,
-    private authorizationService: AuthorizationDataService,
-  ) {
-    this.showSendFeedback$ = this.authorizationService.isAuthorized(FeatureID.CanSendFeedback);
+  constructor(@Optional() private cookies: KlaroService) {
   }
 
   showCookieSettings() {
-    if (hasValue(this.cookies)) {
-      this.cookies.showSettings();
-    }
     return false;
   }
 }
